519727510e1157557fd0f12ee543e4d47c1b7676
Jacob Allen <jballen95173@gmail.com>
2017-09-30 16:01:41 -0400

    Added my file!

--- hardware_applications/week1/starter_code.cpp
+++ hardware_applications/week1/starter_code.cpp
@@ -3,3 +3,40 @@
 
 using namespace std;
 
+int main() {
+    RJRobot robot;
+
+    cout << "Robot is ready to roll!" << endl;
+
+//    robot.SetMotor(MotorPort::A, 200);
+//    robot.SetMotor(MotorPort::B, 200);
+//    robot.Wait(1000ms);
+//    robot.StopMotors();
+
+    robot.SetMotor(MotorPort::A, 200);
+    robot.SetMotor(MotorPort::B, 200);
+    robot.Wait(2600ms);
+
+    robot.SetMotor(MotorPort::A, -200);
+    robot.Wait(2300ms);
+    robot.SetMotor(MotorPort::A, 200);
+    //robot.SetMotor(MotorPort::B, 200);
+    robot.Wait(2400ms);
+    robot.SetMotor(MotorPort::A, -200);
+    robot.Wait(2300ms);
+    robot.SetMotor(MotorPort::A, 200);
+    //robot.SetMotor(MotorPort::B, 200);
+    robot.Wait(2600ms);
+    robot.SetMotor(MotorPort::A, -200);
+    robot.Wait(2300ms);
+    robot.SetMotor(MotorPort::A, 200);
+    //robot.SetMotor(MotorPort::B, 200);
+    robot.Wait(2400ms);
+    robot.StopMotors();
+
+
+
+    return 1;
+
+
+}
